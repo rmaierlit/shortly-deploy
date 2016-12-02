e19ad4d6c3a76fcbcd91d065fcf2e1dcaf56625e
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 16:29:22 -0800

    mongodb refactor, passCompare and make complete

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,45 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
+//mongoose.Promise = Promise;
+var Users = require('../collections/users');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function() {
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function() {
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
+
+var User = mongoose.model('User', Users);
+
+User.prototype.make = function() {
+  var cypher = Promise.promisify(bcrypt.hash);
+  return cypher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
     });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+};
+
+User.prototype.comparePassword = function(attemptedPassword) {
+  var compare = Promise.promisify(bcrypt.compare);
+  return compare(attemptedPassword, this.password);
+};
+
+
 
 module.exports = User;
