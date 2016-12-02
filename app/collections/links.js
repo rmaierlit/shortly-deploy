b6dcf29f52f21e751dc0a28febd3f209095585de
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 16:27:18 -0800

    refactor class to mongodb

--- app/collections/links.js
+++ app/collections/links.js
@@ -1,9 +1,21 @@
 // NOTE: this file is not needed when using MongoDB
 var db = require('../config');
-var Link = require('../models/link');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// var Link = require('../models/link');
 
-var Links = new db.Collection();
+// var Links = new db.Collection();
 
-Links.model = Link;
+// Links.model = Link;
+
+var Links = new Schema({
+  url: {type: String, required: true},
+  baseUrl: {type: String, required: true},
+  code: {type: String, required: true},
+  title: {type: String},
+  visits: {type: Number},
+  'created_at': {type: Date, default: Date.now},
+  'updated_at': {type: Date, default: Date.now}
+});
 
 module.exports = Links;
