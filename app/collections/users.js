f8531f7cd5570ca7a8ae7f93aec5f7632b3ac545
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 16:27:47 -0800

    refactor users to mongodb

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,9 +1,18 @@
 // NOTE: this file is not needed when using MongoDB
 var db = require('../config');
-var User = require('../models/user');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// var User = require('../models/user');
 
-var Users = new db.Collection();
+// var Users = new db.Collection();
 
-Users.model = User;
+// Users.model = User;
+
+var Users = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  'created_at': {type: Date, default: Date.now},
+  'updated_at': {type: Date, default: Date.now}
+});
 
 module.exports = Users;
